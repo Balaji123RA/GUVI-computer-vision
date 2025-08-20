29abb30d8629597db8a7e7c721da37adc08d9aaa
BALAJI R A <148052326+Balaji123RA@users.noreply.github.com>
2025-08-20 11:18:56 +0530

    Delete ex2.py

--- ex2.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-img = cv2.imread("coolie disco.jpeg")
-circle = cv2.circle(img,(150,150),30,(0,0,255),10)
-plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
-plt.axis("off")
-plt.show()
